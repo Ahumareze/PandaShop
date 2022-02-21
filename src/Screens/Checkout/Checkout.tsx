c2ba9c3a8ca098b73d23de8676f49adbcdbf53cd
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-02-21 14:54:32 +0100

    Febuary 21th

--- src/Screens/Checkout/Checkout.tsx
+++ src/Screens/Checkout/Checkout.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FiCheck } from 'react-icons/fi';
 import './Checkout.css';
 
 import CheckoutHeader from './Components/CheckoutHeader';
@@ -7,19 +8,37 @@ import CheckoutInput from './Components/CheckoutInput';
 const width = window.innerWidth;
 
 function Checkout(props: any) {
-    return (
-        <div className='CheckoutPage'>
-            <CheckoutHeader props={props} />
-            <section>
-                <div className='CheckoutForm' style={{width: width - 30}}>
-                    <p className='CheckoutHeader' >Details</p>
-                    <CheckoutInput name='Name' />
-                    <CheckoutInput name='Phone number' />
-                    <CheckoutInput name='Delivery Address' />
-                    <div className='PayOnDeliveryDiv'></div>
-                </div>
-            </section>
+    const [alert, setAlert] = useState(false);
+
+    const view = (
+        <div className='CheckoutAlertDiv'>
+            <div className="CheckoutAlert">
+                <p>Please confirm delivery</p>
+                <div className='AlertButtons'></div>
+            </div>
         </div>
+    )
+
+    return (
+        <>
+            <div className='CheckoutPage'>
+                <CheckoutHeader props={props} />
+                <section>
+                    <div className='CheckoutForm' style={{width: width - 30}}>
+                        <p className='CheckoutHeader' >Details</p>
+                        <CheckoutInput name='Name' />
+                        <CheckoutInput name='Phone number' />
+                        <CheckoutInput name='Delivery Address' />
+                        <div className='PayOnDeliveryDiv'>
+                            <p>Pay on delivery ($300) </p>  
+                            <FiCheck size={20} color='salmon' />
+                        </div>
+                        <button style={{width: width - 60}} >Request Delivery</button>
+                    </div>
+                </section>
+            </div>
+            {view}
+        </>
     );
 }
 
